801eb92096dee428df684297d083a6c8b6267fc8
chikiezienathanbonifacio <chikiezienathanbonifacio@gmail.com>
2026-06-25 15:15:44 +0800

    Add files via upload

---
+++ # CPP102 Week 1 Activity.py bon.py
@@ -0,0 +1,8 @@
+# CPP102 Week 1 Activity
+Name: Chikiezie Nathan B.Bonifacio
+Section: 1 CPE-B
+Course: CPP102 - Programming Logic and Design
+Activity Title: Introduction to Python, Git, and GitHub
+## Reflection
+Git and GitHub are useful tools for programming students because they help track changes in code and make it easy to restore previous versions if mistakes occur They also support collaboration by allowing multiple people to work on the same project and share code efficiently.
+Learning Git and GitHub prepares students for real-world software development, where version control and teamwork are essential skills.
